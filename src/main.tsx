181a31c83e27307287cdf741faa29686dee2c703
Reyabdul <reyabdul@gmail.com>
2025-01-22 15:18:28 +0900

    Add Tanstack Query and Tanstack Dev Tools

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from '@/components/ui/provider'
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import './index.css'
 import App from './App.tsx'
 
+//Create a client
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider>
         <App />
-    </Provider>
+      </Provider>
+      <ReactQueryDevtools />  
+    </QueryClientProvider>
   </StrictMode>,
 )
 
